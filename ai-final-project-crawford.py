26f5baaebb63d8a2633398516ee619192efe1a7d
jillcrawford <jrcrawford@anderson.edu>
2025-04-04 13:41:01 -0400

    getting info from website

--- ai-final-project-crawford.py
+++ ai-final-project-crawford.py
@@ -1,1 +1,12 @@
 # ai-final-project-crawford.py
+# Jillian Crawford
+
+import requests
+from bs4 import BeautifulSoup
+
+# scraping the sudoku puzzle from the internet
+url = 'https://nine.websudoku.com/?level=1'
+web = requests.get(url)
+soup = BeautifulSoup(web.text, 'html.parser')
+
+#
