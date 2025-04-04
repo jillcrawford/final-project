a263cb2d50c265237be57038e5218fe804ad7617
jillcrawford <jrcrawford@anderson.edu>
2025-04-04 13:51:45 -0400

    starting to build out the sudoku puzzle

--- ai-final-project-crawford.py
+++ ai-final-project-crawford.py
@@ -9,4 +9,18 @@ url = 'https://nine.websudoku.com/?level=1'
 web = requests.get(url)
 soup = BeautifulSoup(web.text, 'html.parser')
 
-#
+# filling in the puzzle with the information from the web
+puzzle = []
+# 9 x 9 grid
+for row in range(9):
+    cur_row = []
+    for col in range(9):
+        cellnum = [row][col]
+        cell = soup.find('input', id=cellnum)
+        value = cell.get('value') if cell else '0'
+        cur_row.append(int(value))
+    puzzle.append(cur_row)
+
+# printing out puzzle
+for row in puzzle:
+    print(row)
